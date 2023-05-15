db03454c364f8763984a795af3dcc43bcdf86d07
MP.shikha.pathak.FE <shikha.pathak@mobileprogramming.com>
2023-05-15 09:57:51 +0530

    first commit

---
+++ XamDemo/XamDemo/MyPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamDemo
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+            //commit
+        }
+    }
+}
+
